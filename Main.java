334df3f948bc520e9adf0e11386dccffcad4de81
HoooDyyy <101186790+HoooDyyy@users.noreply.github.com>
2022-03-22 12:24:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+       
+ for(int i=0; i<=50; i+=2)
+        {
+                System.out.println(i);
+        }
+    for(int d=51;d<100; d+=2)
+    {
+                System.out.println(d);
+    }
+    }
+}
